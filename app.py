cdca457ddbff9f82e519561cd5b43337f63de516
Raya Litova <raya.litova23@gmail.com>
2019-06-03 21:41:19 +0300

    umna mu rabota

--- app.py
+++ app.py
@@ -1,19 +1,35 @@
 from flask import Flask, render_template, request
 import os
 app = Flask(__name__)
+filename = 'data.txt'
+
+x=[]
+name=''
+f=open(filename,'r')
+for item in f.read():
+    if '\n'==item:
+        if name in x:
+            name=''
+        else:
+            x.append(name)
+            name=''
+    else:
+        name+=item
+f.close()
 
 poll_data = {
     'question': 'Vote for your favourite team ^^',
-    'fields': ['otbor1', 'otborPesho', 'otborPraskova', 'Nqna', 'uwu']
+    'fields': x
 }
-
-filename = 'data.txt'
 
 
 @app.route('/')
 def root():
     return render_template('poll.html', data=poll_data)
 
+@app.route('/add_temp')
+def add_temp():
+    return render_template('add.html')
 
 @app.route('/poll')
 def poll():
@@ -24,13 +40,12 @@ def poll():
     out.close()
 
     return render_template('thankyou.html', data=vote)
-
-
+    
 @app.route('/results')
 def show_results():
     votes = {}
     for f in poll_data['fields']:
-        votes[f] = 0
+        votes[f] = -1
 
     f = open(filename, 'r')
     for line in f:
@@ -39,6 +54,21 @@ def show_results():
 
     return render_template('results.html', data=poll_data, votes=votes)
 
+@app.route('/add_new', methods=['POST'])
+def add_new():
+    f = open(filename, 'r')
+    if request.form['add'] in f.read():
+        f.close()
+        return 'imeto e zaeto a ti si pedal <3'
+    else:
+        f.close()
+        poll_data['fields'].append(request.form['addnew'])
+        f=open(filename,'a')
+        f.write(request.form['addnew'])
+        f.close()
+        return "Successfully added ^^"
+
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
