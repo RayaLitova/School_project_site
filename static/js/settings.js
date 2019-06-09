4ae66fec1f95eef9c4072c5183eb8a71c3e0714a
Raya Litova <raya.litova23@gmail.com>
2019-06-09 20:21:36 +0300

    added settings

---
+++ static/js/settings.js
@@ -0,0 +1,8 @@
+function Settings(){
+	var current_name=document.getElementById('navbar')
+	};
+function Hidden(){};
+function Navbar(){
+	display=document.getElementById('navbar_display').value
+	if(display==)
+}
