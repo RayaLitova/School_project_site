a781107995fdc148aff1e8cfe953601712c7b123
Iskren <iskren.alexandrov@gmail.com>
2019-06-18 14:33:58 +0300

    added border to id field

--- static/js/custom_settings.js
+++ static/js/custom_settings.js
@@ -1,4 +1,8 @@
 document.body.style.backgroundColor = localStorage.page;
-document.getElementById('navbar').style.backgroundColor=localStorage.nav_color;
-document.getElementById('h1').style.color=localStorage.txt;
-document.getElementById('results').style.color=localStorage.txt;
+document.getElementById('navbar').style.backgroundColor = localStorage.nav_color;
+document.getElementById('results').style.color = localStorage.txt;
+document.getElementById('field').style.color = localStorage.txt;
+let txt = document.getElementsByClassName("h1");
+for (x <= txt.length; x = 0; x += 1) {
+    txt[x].style.color = localStorage.txt;
+};
